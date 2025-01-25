1bd5fe63298b4260d3da35da89c35005bfd20df8
alexzanderallen01 <49131555+alexzanderallen01@users.noreply.github.com>
2025-01-25 15:55:20 -0700

    Completed base program. Functions work with no errors. Needed impoves are in README.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        //Created a 2d array called Board to store board state in table format.
+        string[,] Board = new string[3, 3];
+        //max # of rows
+        int rows = 3;
+        //max nuber of columns
+        int columns = 3; 
+        //player variables to make detirming which symbol to use on game board simple.
+        string player1 = "x";
+        string player2 = "o";
+        Console.WriteLine("Tic Tac Toe Board:");
+
+        Setupgame(Board, rows, columns, player1, player2);
+        printBoard(Board, rows, columns);
+
+        //loop to checks if player a player has won. If not keep playing. (remake this to call gameover inside to allow for game to be restarted)
+        while (!GameOver(Board, player1) && !GameOver(Board, player2)) {
+            PlayGame(Board, rows, columns, player1, player2);
+        }
+    }
+
+    //function that will fill up the 2d array with an empty
+    public static void Setupgame(string[,] Board, int rows, int columns, string player1, string player2) {
+        //create out empty board to be printed by create board
+        for (var i = 0; i < rows; i++) {
+            for (var o = 0; o < columns; o++) {
+                Board[i,o] = ".";
+            }
+        }
+    }
+
+    //function that will take in 2 int varibles to print the tic tac toe board. Should allow for the board to be scaled to any size if I have the time for that.
+    public static void printBoard(string[,] Board, int rows, int columns) {
+        //print board with player changes
+        for (var i = 0; i < rows; i++) {
+            for (var o = 0; o < columns; o++) {
+                Console.Write(Board[i,o]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    //function that will take in all needed info & use call functions needed to run the game loop
+    public static void PlayGame(string[,] Board, int rows, int columns, string player1, string player2) {
+        //get player 1 turn, print their change. Then do that for player 2.
+        SetPosition(Board, player1);
+        printBoard(Board, rows, columns);
+        SetPosition(Board, player2);
+        printBoard(Board, rows, columns);
+    }
+
+    //function that will ask the current play the index of the row & colomn of the spot they want to pick & set that spot to their symbol
+    public static void SetPosition(string[,] Board, string player) {
+        //Get row & column indexs of spot they want to pick
+        Console.WriteLine($"player {player} choose a row (1-3):");
+        int playerRow = Convert.ToInt32(Console.ReadLine()) - 1;
+        Console.WriteLine($"player {player} choose a column (1-3):");
+        int playerColumn = Convert.ToInt32(Console.ReadLine()) - 1;
+        //use row index & column index in 2d array to change player choose spot on board to player symbol
+        Board[playerRow, playerColumn] = player;
+    }
+
+    //function that will read game board & check all posibles areas to see if a player has won.
+    //currently does not work for game over "cat". Need to re make this to take in both players & check if there is no more "." on the game board
+    public static bool GameOver(string[,] Board, string player) {
+        //every variable need to check every winniable area by pulling all indexes that would effect that area of win.
+        string TopRow = Board[0, 0] + Board[0, 1] + Board[0, 2];
+        string MiddleRow = Board[1, 0] + Board[1, 1] + Board[1, 2];
+        string BottomRow = Board[2, 0] + Board[2, 1] + Board[2, 2];
+        string FirstColomn = Board[0, 0] + Board[1, 0] + Board[2, 0];
+        string SecondColpmn = Board[0, 1] + Board[1, 1] + Board[2, 1];
+        string ThirdColomn = Board[0, 2] + Board[1, 2] + Board[2, 2];
+        string Diagon = Board[0, 0] + Board[1, 1] + Board[2, 2];
+        string OtherDiagon = Board[0, 2] + Board[1, 1] + Board[2, 0];
+
+        string playerWinCondition = player + player + player;
+
+        //Check every spot a player can win to see if they have won
+        if (TopRow == playerWinCondition || 
+        MiddleRow == playerWinCondition || 
+        BottomRow == playerWinCondition || 
+        FirstColomn == playerWinCondition || 
+        SecondColpmn == playerWinCondition || 
+        ThirdColomn == playerWinCondition || 
+        Diagon == playerWinCondition || 
+        OtherDiagon == playerWinCondition) {
+            if (player == "x") {
+                Console.WriteLine($"player 1 wins!");
+            }
+            else {
+                Console.WriteLine($"player 2 wins!");
+            }
+            
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Overview
+
+This is a C# tic tac toe consle program I created to help me not only get back into the flow of C#, but to research different ways I can go about make a simple program. After a lot of research I decided to base this program of 2d arrays to allow me to make this program easily scaliable & allow me to be able to add a lot of features to the game. The hope was to create a tic tac toe program that could easily be scaled to the size of a checkers board & if I had enough time, have multible game modes
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+tools that I used to develop the software:
+The 2 tools I used where VS code to write my code in & check for bugs & typos, and W3school's try it your self feature to test small pieces on code to better understand how it worked.
+
+the programming language that I used and any libraries:
+I used c# which is a general-purpose, object-oriented programming language used to create software. I did not use any other libraries in this program as I started this project believe I had all the tools I needed in VS code after my starting research.
+
+# Useful Websites
+
+- [W3schools](https://www.w3schools.com/cs/index.php): This is where I spent half of my time while researching as it not only has a lot of information, but also lets me try code & remember the difference in syntax through the try it your self feature
+- [stackoverflow](https://stackoverflow.com): This was the form I gathered a lot of ideas from that I learned that 2d arrays are a thing from
+
+# Future Work
+list of things that you need to fix, improve, and add in the future:
+
+- Fix player 2 being able to take their turn after player 1 wins
+- make it so that player can not over ride a spot that already has been writen on.
+- impove gameover funtion to detect if that game ended in a cat

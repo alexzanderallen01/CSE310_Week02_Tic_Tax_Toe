50912d0d9b5f07df3d0e0b30ef15e4727d8f1b7b
alexzanderallen01 <49131555+alexzanderallen01@users.noreply.github.com>
2025-01-25 19:22:28 -0700

    Finished youtube video on program & added it to README

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566bc3e24cc09a57efcd22098700e7ee9b9234fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd5fe63298b4260d3da35da89c35005bfd20df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
